03fa104c879611a4b76daca20a5c726451ea1a9a
M.Stupishina <margaritastupishina@rocketmail.com>
2026-05-24 17:02:59 +0300

    dataset.py для разбиения датасета со стратификацией для классификации

---
+++ src/telco_churn/dataset.py
@@ -0,0 +1,52 @@
+import pandas as pd
+from typing import Tuple
+
+from configs.Telco_churn_config import Config
+from src.telco_churn.utils import load_data, save_data
+
+
+class DatasetLoader:
+    """
+    Класс для загрузки и разбиения датасета на train/val/test со стратификацией для классфикации.
+    """
+    def __init__(self, config: Config):
+        self.config = config
+
+    def load_and_split_data(
+            self
+    ) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+        """
+        Загрузка и разбиение данных на train/val/test со стратификацией
+        """
+        df = load_data(self.config.raw_data_path)
+
+        y = df[self.config.target_column]
+
+        train_val_df, test_df = train_test_split(
+            df, test_size=self.config.test_size, stratify=y, random_state=self.config.random_state)
+
+        y_train_val = train_val_df[self.config.target_column]
+
+        train_df, val_df = train_test_split(
+            train_val_df,
+            test_size=self.config.val_size,
+            stratify=y_train_val,
+            random_state=self.config.random_state)
+
+        return train_df, val_df, test_df
+
+    def save_splits(
+            self,
+            train_df: pd.DataFrame,
+            val_df: pd.DataFrame,
+            test_df: pd.DataFrame,
+    ) -> None:
+        """Сохранение сплитов в CSV файлы с использованием utils.save_data"""
+        output_dir = self.config.processed_data_dir
+        output_dir.mkdir(parents=True, exist_ok=True)
+
+        save_data(train_df, output_dir / "train.csv")
+        save_data(val_df, output_dir / "val.csv")
+        save_data(test_df, output_dir / "test.csv")
+
+
