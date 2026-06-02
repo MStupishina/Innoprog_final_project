a8b8fd11dff175131ed55a55edb9835f82a90cd2
M.Stupishina <margaritastupishina@rocketmail.com>
2026-06-02 21:48:09 +0300

    корректировка тюнинга для работы с пайплайном препроцессор+модель

--- src/telco_churn/tuning.py
+++ src/telco_churn/tuning.py
@@ -3,12 +3,12 @@ from typing import Dict, Any
 import numpy as np
 import optuna
 from optuna.integration import LightGBMPruningCallback
-from lightgbm import LGBMClassifier, early_stopping, log_evaluation
+from lightgbm import early_stopping, log_evaluation
+from sklearn import clone
 from sklearn.metrics import roc_auc_score, f1_score
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import Pipeline
 
 from configs.Telco_churn_config import Config
-
 
 class LGBMTuner:
     """Подбор гиперпараметров для LightGBM"""
@@ -22,32 +22,39 @@ class LGBMTuner:
     def _suggest_params(self, trial: optuna.Trial) -> Dict[str, Any]:
 
         params = {
-            "n_estimators": 1000,
-            "learning_rate": trial.suggest_float("learning_rate", 0.01, 0.15),
-            "num_leaves": trial.suggest_int("num_leaves", 20, 80),
-            "max_depth": trial.suggest_int("max_depth", 3, 10),
-            "min_child_samples": trial.suggest_int("min_child_samples", 10, 100),
-            "subsample": trial.suggest_float("subsample", 0.6, 1.0),
-            "colsample_bytree": trial.suggest_float("colsample_bytree", 0.6, 1.0),
-            "reg_alpha": trial.suggest_float("reg_alpha", 1e-3, 10, log=True),
-            "reg_lambda": trial.suggest_float("reg_lambda", 1e-3, 10, log=True),
-            "scale_pos_weight": trial.suggest_float("scale_pos_weight", 1, 10),
-            "random_state": self.random_state,
-            "verbosity": -1,
+            "model__n_estimators": 1000,
+            "model__learning_rate": trial.suggest_float("learning_rate", 0.01, 0.15),
+            "model__num_leaves": trial.suggest_int("num_leaves", 20, 80),
+            "model__max_depth": trial.suggest_int("max_depth", 3, 10),
+            "model__min_child_samples": trial.suggest_int("min_child_samples", 10, 100),
+            "model__subsample": trial.suggest_float("subsample", 0.6, 1.0),
+            "model__colsample_bytree": trial.suggest_float("colsample_bytree", 0.6, 1.0),
+            "model__reg_alpha": trial.suggest_float("reg_alpha", 1e-3, 10, log=True),
+            "model__reg_lambda": trial.suggest_float("reg_lambda", 1e-3, 10, log=True),
+            "model__scale_pos_weight": trial.suggest_float("scale_pos_weight", 1, 10),
+            "model__random_state": self.random_state,
+            "model__verbosity": -1,
         }
 
         return params
 
 
-    def _objective(self, trial: optuna.Trial, X_train: np.ndarray, y_train: np.ndarray,
+    def _objective(self, trial: optuna.Trial, pipeline: Pipeline, X_train: np.ndarray, y_train: np.ndarray,
                    X_val: np.ndarray, y_val: np.ndarray) -> float:
 
         params = self._suggest_params(trial)
-        model = LGBMClassifier(**params)
+        preprocessor = clone(pipeline.named_steps["preprocessor"])
+        X_train_processed = preprocessor.fit_transform(X_train)
+        X_val_processed = preprocessor.transform(X_val)
+        model = clone(pipeline.named_steps["model"])
+        model.set_params(**{
+            k.replace("model__", ""): v
+            for k, v in params.items()
+        })
         model.fit(
-            X_train,
+            X_train_processed,
             y_train,
-            eval_set=[(X_val, y_val)],
+            eval_set=[(X_val_processed, y_val)],
             eval_metric=self.metric_name,
             callbacks=[
                 LightGBMPruningCallback(trial, self.metric_name),
@@ -55,19 +62,22 @@ class LGBMTuner:
                 early_stopping(stopping_rounds=50),  # стандартная ранняя остановка
                 log_evaluation(period=0),
             ])
-        proba = model.predict_proba(X_val)[:, 1]
-        score = roc_auc_score(y_val, proba)
-        return score
+        proba = model.predict_proba(X_val_processed)[:, 1]
 
-    def tune(self, X_train: np.ndarray, y_train: np.ndarray,
+        return roc_auc_score(y_val, proba)
+
+    def tune(self, pipeline: Pipeline, X_train: np.ndarray, y_train: np.ndarray,
              X_val: np.ndarray, y_val: np.ndarray) -> Dict[str, Any]:
         sampler = optuna.samplers.TPESampler(seed=self.random_state)
         study = optuna.create_study(direction="maximize", sampler=sampler, study_name="lgbm_classification_tuning")
         study.optimize(
-            lambda trial: self._objective(trial, X_train, y_train, X_val, y_val),
+            lambda trial: self._objective(trial, pipeline, X_train, y_train, X_val, y_val),
                        n_trials=self.n_trials)
 
-        best_params = self._suggest_params(study.best_trial)
+        best_params = {f"model__{k}": v for k, v in study.best_params.items()}
+        best_params["model__n_estimators"] = 1000
+        best_params["model__random_state"] = self.random_state
+        best_params["model__verbosity"] = -1
         print("Best LGBM params:", best_params)
         return best_params
 
@@ -82,29 +92,31 @@ class KNNTuner:
 
     def _suggest_params(self, trial: optuna.Trial) -> Dict[str, Any]:
         params = {
-            "n_neighbors": trial.suggest_int("n_neighbors", 3, 20),
-            "weights": trial.suggest_categorical("weights", ["uniform", "distance"]),
-            "p": trial.suggest_categorical("p", [1, 2]),  # 1 = Manhattan, 2 = Euclidean
-            "algorithm": trial.suggest_categorical("algorithm", ["auto", "ball_tree", "kd_tree", "brute"]),
-            "leaf_size": trial.suggest_int("leaf_size", 20, 50)
+            "model__n_neighbors": trial.suggest_int("n_neighbors", 3, 20),
+            "model__weights": trial.suggest_categorical("weights", ["uniform", "distance"]),
+            "model__p": trial.suggest_categorical("p", [1, 2]),  # 1 = Manhattan, 2 = Euclidean
+            "model__algorithm": trial.suggest_categorical("algorithm", ["auto", "ball_tree", "kd_tree", "brute"]),
+            "model__leaf_size": trial.suggest_int("leaf_size", 20, 50)
         }
         return params
 
-    def _objective(self, trial: optuna.Trial, X_train: np.ndarray, y_train: np.ndarray,
+    def _objective(self, trial: optuna.Trial, pipeline: Pipeline, X_train: np.ndarray, y_train: np.ndarray,
                    X_val: np.ndarray, y_val: np.ndarray) -> float:
         params = self._suggest_params(trial)
-        model = KNeighborsClassifier(**params)
-        model.fit(X_train, y_train)
-        y_proba = model.predict_proba(X_val)[:, 1]
+        trial_pipeline = clone(pipeline)
+        trial_pipeline.set_params(**params)
+        trial_pipeline.fit(X_train, y_train)
+        y_proba = trial_pipeline.predict_proba(X_val)[:, 1]
         y_pred = (y_proba >= 0.5).astype(int)
-        score = f1_score(y_val, y_pred)
-        return score
 
-    def tune(self, X_train: np.ndarray, y_train: np.ndarray,
+        return f1_score(y_val, y_pred)
+
+    def tune(self, pipeline: Pipeline, X_train: np.ndarray, y_train: np.ndarray,
              X_val: np.ndarray, y_val: np.ndarray) -> Dict[str, Any]:
         sampler = optuna.samplers.TPESampler(seed=self.random_state)
         study = optuna.create_study(direction="maximize", sampler=sampler, study_name="knn_classification_tuning")
-        study.optimize(lambda trial: self._objective(trial, X_train, y_train, X_val, y_val),
+        study.optimize(lambda trial: self._objective(trial, pipeline, X_train, y_train, X_val, y_val),
                        n_trials=self.n_trials)
-        print("Best KNN params:", study.best_params)
-        return study.best_params
+        best_params = {f"model__{k}": v for k, v in study.best_params.items()}
+        print("Best KNN params:", best_params)
+        return best_params
