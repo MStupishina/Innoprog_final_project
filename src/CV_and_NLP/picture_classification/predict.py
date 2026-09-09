4d0d4bf03db1fea5d658e999b1a3755a675153a4
M.Stupishina <margaritastupishina@rocketmail.com>
2026-09-09 20:45:15 +0300

    инференс и анализ ошибок для классификации изображений

--- src/CV_and_NLP/picture_classification/predict.py
+++ src/CV_and_NLP/picture_classification/predict.py
@@ -1,11 +1,11 @@
 import argparse
 
 from configs.cv_and_nlp_config import Config
-from predictor import load_model, predict_image
+from predictor import load_model, predict_image, load_threshold
 
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="Инференс для классификации изображений")
     parser.add_argument("--image", required=True)
     parser.add_argument("--model",
                         default="artifacts/cv_classification/resnet18/best_model.pt"
@@ -13,11 +13,27 @@ def main():
     args = parser.parse_args()
     config = Config()
     model = load_model(config, args.model)
-    classes, probs = predict_image(model, args.image, config)
+    threshold = load_threshold(config=config, model_path=args.model)
+    classes, probs = predict_image(
+        model=model,
+        image_path=args.image,
+        config=config,
+        threshold=threshold,
+    )
 
-    print("Predicted classes:")
-    for c in classes:
-        print("-", c)
+    print(f"Model: ResNet18")
+    print(f"Threshold: {threshold:.3f}")
+
+    print("\nPredictions:")
+
+    if not classes:
+        print("No classes detected above threshold.")
+    else:
+        for idx, probability in enumerate(probs):
+            if probability > threshold:
+                class_name = config.B1["classes"][idx]
+                print(f"- {class_name}: {probability:.3f}")
+
 
 if __name__ == "__main__":
     main()
